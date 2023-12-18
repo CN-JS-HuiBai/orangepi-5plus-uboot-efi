d03711c0bfff027a55733b0ec0df189dc9f96030
CN-JS-HuiBai <h208434317@gmail.com>
2023-12-18 22:47:28 +0800

    fix building problems

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt update
 apt install -y gcc-aarch64-linux-gnu swig python3-pyelftools make gcc cpp libncurses-dev libssl-dev
-apt install -y git fakeroot libncurses-dev zstd dracut bc flex bison dwarves
+apt install -y git fakeroot libncurses-dev zstd bc flex bison dwarves
 make mrproper
 cp --recursive rkbin /tmp 
 export ROCKCHIP_TPL=/tmp/rkbin/rk35/rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.12.bin
